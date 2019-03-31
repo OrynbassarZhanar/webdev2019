9dad2d8a044e33cbd2715c38b418640e8cab19e6
OrynbassarZhanar <zhanar991025@gmail.com>
2019-03-31 21:18:05 +0300

    week10

---
+++ week 10/Informatics/5a.py
@@ -0,0 +1,10 @@
+def find_min(a,b,c,d):
+    print(min(a,b,c,d),end="")
+def main():
+    a=int(input())
+    b=int(input())
+    c=int(input())
+    d=int(input())
+    find_min(a,b,c,d)
+if __name__ == "__main__":
+    main()
---
+++ week 10/Informatics/5b.py
@@ -0,0 +1,8 @@
+def find_power(a,b):
+    print(pow(a,b),end="")
+def main():
+    a=float(input())
+    b=int(input())
+    find_power(a,b)
+if __name__ == "__main__":
+    main()
