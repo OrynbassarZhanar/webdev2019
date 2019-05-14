f94f06a782204a532f1a2520beb381b4eabb65bd
OrynbassarZhanar <zhanar991025@gmail.com>
2019-05-14 08:32:39 +0300

    Zhanar

--- quiz/quiz-back/quizenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\zhanar\desktop\webka\quiz\quiz-back\quizenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
